a6b495f8549ef51dc27d5a9000ea6ca9e8f611a1
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-10-22 14:16:39 +0200

    Add scripts

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+forge clean
+forge build
+forge coverage --report summary --report lcov
+genhtml lcov.info -o report --branch-coverage --ignore-errors inconsistent,corrupt lcov.info
+xdg-open report/index.html
