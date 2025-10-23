69e0bd9d5cb20b982957ebef5f9a6070c94ffe89
Kacper Żuk <kacper.zuk@neti-soft.com>
2025-10-23 11:10:21 +0200

    CI Fixes

--- ci/check-abis.sh
+++ ci/check-abis.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 cd "$(dirname "$0")"/..
 
-for i in src/*.sol src/**/*.sol; do
-	i="$(basename "$i")"
-	name="${i%.sol}"
-	[ -f "abis/${name}.json" ] || exit 1
-	{ jq .abi < "out/$i/$name.json" | diff - "abis/${name}.json" >/dev/null; } || exit 1
+for i in $(find src -name '*.sol'); do
+  i="$(basename "$i")"
+  name="${i%.sol}"
+  [ -f "abis/${name}.json" ] || exit 1
+  { jq .abi <"out/$i/$name.json" | diff - "abis/${name}.json" >/dev/null; } || exit 1
 done
--- gen-abis.sh
+++ gen-abis.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 rm -rf abis
 mkdir abis
 
-for i in src/*.sol src/**/*.sol; do
-	i="$(basename "$i")"
-	name="${i%.sol}"
-	jq .abi < "out/$i/$name.json" > "abis/${name}.json"
+for i in $(find src -name '*.sol'); do
+  echo "Generating ABI file for $i"
+  i="$(basename "$i")"
+  name="${i%.sol}"
+  jq .abi <"out/$i/$name.json" >"abis/${name}.json"
 done
