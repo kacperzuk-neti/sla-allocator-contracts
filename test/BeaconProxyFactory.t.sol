7f8714b7d8d5ebcd2ab8cb7e8473ba530ff9b612
rmagrys <ra.magrys@gmail.com>
2025-10-24 09:54:30 +0200

    tests

---
+++ test/BeaconProxyFactory.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {BeaconProxyFactory} from "../src/BeaconProxyFactory.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Beneficiary} from "../src/Beneficiary.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+contract BeaconProxyFactoryTest is Test {
+    BeaconProxyFactory public factory;
+    address public impl;
+    address public manager;
+
+    function setUp() public {
+        manager = vm.addr(1);
+        impl = address(new Beneficiary());
+        factory = new BeaconProxyFactory(manager, impl);
+    }
+
+    function testDeployEmitsEvent() public {
+        vm.expectEmit(false, false, false, true);
+        emit BeaconProxyFactory.ProxyCreated(vm.addr(1));
+        factory.create(manager);
+    }
+
+    function testDeployIncrementsNonce() public {
+        factory.create(manager);
+        assertEq(factory.nonce(manager), 1);
+        factory.create(manager);
+        assertEq(factory.nonce(manager), 2);
+    }
+
+    function testDeployProxyAddedToMapping() public {
+        factory.create(manager);
+        assertTrue(factory.proxyDeployed(vm.addr(1)));
+    }
+
+    function testDeployContract() public {
+        address addr = address(new BeaconProxyFactory(manager, impl));
+        uint32 size;
+
+        assembly {
+            size := extcodesize(addr)
+        }
+        assertGt(size, 0);
+    }
+}
