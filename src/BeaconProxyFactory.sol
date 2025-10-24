73422c56cf24422faeebd658d2df5feea2bcc1b8
rmagrys <ra.magrys@gmail.com>
2025-10-24 10:19:19 +0200

    solhint fixes

--- src/BeaconProxyFactory.sol
+++ src/BeaconProxyFactory.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
--- test/BeaconProxyFactory.t.sol
+++ test/BeaconProxyFactory.t.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {BeaconProxyFactory} from "../src/BeaconProxyFactory.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {Beneficiary} from "../src/Beneficiary.sol";
-import {Vm} from "forge-std/Vm.sol";
-import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 
 contract BeaconProxyFactoryTest is Test {
     BeaconProxyFactory public factory;
