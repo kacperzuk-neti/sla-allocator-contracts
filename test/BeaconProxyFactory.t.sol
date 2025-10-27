a3bd534fc84ea93a973ddcc68257dd4690d3cb7c
rmagrys <ra.magrys@gmail.com>
2025-10-27 12:18:59 +0100

    cr fixes

--- test/BeaconProxyFactory.t.sol
+++ test/BeaconProxyFactory.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {BeaconProxyFactory} from "../src/BeaconProxyFactory.sol";
 import {Beneficiary} from "../src/Beneficiary.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
 contract BeaconProxyFactoryTest is Test {
     BeaconProxyFactory public factory;
@@ -16,9 +17,17 @@ contract BeaconProxyFactoryTest is Test {
         factory = new BeaconProxyFactory(manager, impl);
     }
 
+    function testEmitsUpgradedInConstructor() public {
+        vm.expectEmit(true, false, false, true);
+        emit UpgradeableBeacon.Upgraded(impl);
+
+        new BeaconProxyFactory(manager, impl);
+    }
+
     function testDeployEmitsEvent() public {
-        vm.expectEmit(false, false, false, true);
+        vm.expectEmit(false, true, false, true);
         emit BeaconProxyFactory.ProxyCreated(vm.addr(1));
+
         factory.create(manager);
     }
 
@@ -27,15 +36,5 @@ contract BeaconProxyFactoryTest is Test {
         assertEq(factory.nonce(manager), 1);
         factory.create(manager);
         assertEq(factory.nonce(manager), 2);
-    }
-
-    function testDeployContract() public {
-        address addr = address(new BeaconProxyFactory(manager, impl));
-        uint32 size;
-
-        assembly {
-            size := extcodesize(addr)
-        }
-        assertGt(size, 0);
     }
 }
