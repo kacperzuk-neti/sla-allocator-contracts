4063e8a365d5b6bc8da0b0f302e6fc4abf12f0d8
Filip-L <lelekfilip@gmail.com>
2025-10-28 06:37:11 +0100

    Add get beneficiary

--- test/Beneficiary.t.sol
+++ test/Beneficiary.t.sol
@@ -4,11 +4,25 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {Beneficiary} from "../src/Beneficiary.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BuiltinActorsMock} from "../test/contracts/BuiltinActorsMock.sol";
+import {MockProxy} from "../test/contracts/MockProxy.sol";
+import {MinerTypes} from "filecoin-solidity/v0.8/types/MinerTypes.sol";
 
 contract BeneficiaryTest is Test {
     Beneficiary public beneficiary;
+    BuiltinActorsMock public builtinActorsMock;
+    address public constant CALL_ACTOR_ID = 0xfe00000000000000000000000000000000000005;
+
+    uint64 SP1 = 10000;
+    uint64 SP2 = 20000;
+    uint64 SP3 = 30000;
 
     function setUp() public {
+        builtinActorsMock = new BuiltinActorsMock();
+        address mockProxy = address(new MockProxy());
+        vm.etch(CALL_ACTOR_ID, address(mockProxy).code);
+        vm.etch(address(5555), address(builtinActorsMock).code);
+
         Beneficiary impl = new Beneficiary();
         bytes memory initData = abi.encodeWithSignature("initialize(address)", address(this));
         ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
@@ -18,5 +32,30 @@ contract BeneficiaryTest is Test {
     function testIsAdminSet() public {
         bytes32 adminRole = beneficiary.DEFAULT_ADMIN_ROLE();
         assertTrue(beneficiary.hasRole(adminRole, address(this)));
+    }
+
+    function testGetBeneficiaryForSP1() public {
+        MinerTypes.GetBeneficiaryReturn memory result = beneficiary.getBeneficiary(SP1);
+        assertEq(result.active.beneficiary.data, hex"00904E");
+    }
+
+    function testGetBeneficiaryForSP2() public {
+        MinerTypes.GetBeneficiaryReturn memory result = beneficiary.getBeneficiary(SP2);
+        assertEq(result.active.beneficiary.data, hex"00B8C101");
+    }
+
+    function testGetBeneficiaryForSP3() public {
+        MinerTypes.GetBeneficiaryReturn memory result = beneficiary.getBeneficiary(SP3);
+        assertEq(result.active.beneficiary.data, hex"00C2A101");
+    }
+
+    function testGetBeneficiaryPendingChangeForSP3() public {
+        MinerTypes.GetBeneficiaryReturn memory result = beneficiary.getBeneficiary(SP3);
+        assertEq(result.proposed.new_beneficiary.data, hex"00D4C101");
+    }
+
+    function testGetBeneficiaryExpectRevertExitCodeError() public {
+        vm.expectRevert(abi.encodeWithSelector(Beneficiary.ExitCodeError.selector));
+        beneficiary.getBeneficiary(12345);
     }
 }
---
+++ test/contracts/BuiltinActorsMock.sol
@@ -0,0 +1,42 @@
+// solhint-disable use-natspec
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+contract BuiltinActorsMock {
+    error MethodNotFound();
+
+    // solhint-disable-next-line no-complex-fallback
+    fallback(bytes calldata data) external returns (bytes memory) {
+        (uint256 methodNum,,,,, uint64 target) = abi.decode(data, (uint64, uint256, uint64, uint64, bytes, uint64));
+        if (methodNum == 4158972569 && target == 10000) {
+            return abi.encode(0, 0x51, hex"82824300904E83404000F6");
+        }
+        if (methodNum == 4158972569 && target == 20000) {
+            return abi.encode(0, 0x51, hex"82824400B8C101834203E8410A1A005B8D80F6");
+        }
+        if (methodNum == 4158972569 && target == 30000) {
+            return abi.encode(0, 0x51, hex"82824400C2A101834203E8410A1A005B8D80854400D4C1014207D01A006ACFC0F5F4");
+        }
+        if (methodNum == 4158972569 && target == 12345) {
+            // Simulate error exit code
+            return abi.encode(1, 0x51, hex"82824083404000F6");
+        }
+        revert MethodNotFound();
+    }
+}
+
+// SP1 - 10000 - 82824400a58b0183404000f6 - same as owner
+// GetBeneficiaryReturn {
+//      ActiveBeneficiary active; beneficiary = f010000 term { quota: 0, used_quota: 0, expiration: 0 }
+//      PendingBeneficiaryChange proposed; = null
+
+// SP2 - 20000 - 82824400B8C101834203E8410A1A005B8D80F6
+// GetBeneficiaryReturn {
+//      ActiveBeneficiary active; beneficiary = f024760 term { quota: 1000, used_quota: 10, expiration: 6000000 }
+//      PendingBeneficiaryChange proposed; = null
+
+// SP3 - 30000 - 82824400C2A101834203E8410A1A005B8D80854400D4C1014207D01A006ACFC0F5F4
+// GetBeneficiaryReturn {
+//      ActiveBeneficiary active; beneficiary = f020674 term { quota: 1000, used_quota: 10, expiration: 6000000 }
+//      PendingBeneficiaryChange proposed; = { beneficiary = f024788, new_quota: 2000, new_expiration: 7000000, approved_by_beneficiary: true, approved_by_nominee: false } }
