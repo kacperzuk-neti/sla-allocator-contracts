f93f390adbbd998905cbb77070c50ccab106672f
rmagrys <ra.magrys@gmail.com>
2025-11-10 13:45:03 +0100

    slashRecipient => burnAddress update

--- src/Beneficiary.sol
+++ src/Beneficiary.sol
@@ -39,15 +39,15 @@ contract Beneficiary is Initializable, AccessControlUpgradeable {
     SLAAllocator public slaAllocator;
 
     /**
-     * @notice The address to set as the slash recipient.
+     * @notice The address to set as the burn address.
      */
-    address public slashRecipient;
+    address public burnAddress;
 
     /**
-     * @notice Emits a SlashRecipientUpdated event.
-     * @param slashRecipient The address to set as the slash recipient.
+     * @notice Emits a BurnAddressUpdated event.
+     * @param burnAddress The address to set as the burn address.
      */
-    event SlashRecipientUpdated(address indexed slashRecipient);
+    event BurnAddressUpdated(address indexed burnAddress);
 
     // solhint-disable gas-indexed-events
     /**
@@ -97,14 +97,14 @@ contract Beneficiary is Initializable, AccessControlUpgradeable {
      * @param manager Address of the contract manager
      * @param provider_ Address of the storage provider
      * @param slaAllocator_ Address of the SLA registry contract
-     * @param burnAddress Address of the burn address
+     * @param burnAddress_ Address of the burn address
      */
     function initialize(
         address admin,
         address manager,
         CommonTypes.FilActorId provider_,
         SLAAllocator slaAllocator_,
-        address burnAddress
+        address burnAddress_
     ) external initializer {
         __AccessControl_init();
         _setRoleAdmin(WITHDRAWER_ROLE, MANAGER_ROLE);
@@ -114,17 +114,17 @@ contract Beneficiary is Initializable, AccessControlUpgradeable {
 
         provider = provider_;
         slaAllocator = slaAllocator_;
-        slashRecipient = burnAddress;
+        burnAddress = burnAddress_;
     }
 
     /**
-     * @notice Emits a SlashRecipientUpdated event.
-     * @param slashRecipient_ The address to set as the slash recipient.
+     * @notice Emits a BurnAddressUpdated event.
+     * @param newBurnAddress The address to set as the burn address.
      * @dev Only the admin can set the slash recipient.
      */
-    function setSlashRecipient(address slashRecipient_) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        slashRecipient = slashRecipient_;
-        emit SlashRecipientUpdated(slashRecipient_);
+    function setBurnAddress(address newBurnAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        burnAddress = newBurnAddress;
+        emit BurnAddressUpdated(newBurnAddress);
     }
 
     /**
