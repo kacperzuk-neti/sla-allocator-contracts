38a2146b1ac403984436b9dc9d2ef1f0cbe45489
Szooot <michalos277@gmail.com>
2025-12-17 00:15:54 +0100

    feat: sp owner mock for test purpose

--- test/SLAAllocator.t.sol
+++ test/SLAAllocator.t.sol
@@ -544,6 +544,11 @@ contract SLAAllocatorTest is Test {
     function testRequestDataCapAttestationAlreadyUsedRevert() public {
         resolveAddress.setId(address(this), uint64(20000));
         resolveAddress.setAddress(hex"00C2A101", uint64(20000));
+    function testRequestDataCapWithNoPasspportEmitEvent() public {
+        resolveAddress.setId(address(this), uint64(20000));
+        resolveAddress.setAddress(hex"00C2A101", uint64(20000));
+        resolveAddress.setAddress(hex"f101", uint64(123));
+
         address beneficiaryEthAddressContract = FilAddressIdConverter.toAddress(20000);
         mockBeneficiaryFactory.setInstance(SP2, beneficiaryEthAddressContract);
 
@@ -554,6 +559,14 @@ contract SLAAllocatorTest is Test {
         });
 
         bytes32 structHash = verifySignaturesHelper.hashManualAttestationExt(attestation);
+        SLAAllocator.PaymentTransaction memory txn = SLAAllocator.PaymentTransaction({
+            id: bytes("1"),
+            from: CommonTypes.FilAddress({data: hex"f101"}),
+            to: CommonTypes.FilAddress({data: hex"f102"}),
+            amount: 1
+        });
+
+        bytes32 structHash = verifySignaturesHelper.hashPaymentTransactionExt(txn);
         bytes32 digest = verifySignaturesHelper.digestToSignExt(structHash);
 
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(attestorKey, digest);
@@ -608,6 +621,8 @@ contract SLAAllocatorTest is Test {
 
         SLAAllocator.ManualAttestationSigned memory signedAttestation =
             SLAAllocator.ManualAttestationSigned({attestation: attestation, signature: signature});
+        SLAAllocator.PaymentTransactionSigned memory signedTxn =
+            SLAAllocator.PaymentTransactionSigned({txn: txn, signature: signature});
 
         vm.prank(client);
         vm.expectEmit(true, true, false, true);
@@ -653,5 +668,6 @@ contract SLAAllocatorTest is Test {
         vm.prank(address(this));
         vm.expectRevert(SLAAllocator.SLAAlreadyRegistered.selector);
         verifySignaturesHelper.requestDataCap(address(slaRegistry), signedAttestation2);
+        verifySignaturesHelper.requestDataCap(SP2, address(slaRegistry), 1, signedTxn);
     }
 }
--- test/contracts/ActorIdMock.sol
+++ test/contracts/ActorIdMock.sol
@@ -112,6 +112,11 @@ contract ActorIdMock {
         if (methodNum == 3916220144) {
             return handleAddVerifiedClient();
         }
+        if (methodNum == 3275365574) {
+            if (target == 20000) {
+                return abi.encode(0, 0x51, hex"824400C2A101f6");
+            }
+        }
         if (target == 6 && methodNum == 2199871187) {
             // verifreg get claims
             return abi.encode(0, 0x51, _getClaimsResult);
@@ -121,9 +126,6 @@ contract ActorIdMock {
             // return abi.encode(0, 0x51, hex"83808083410141024103");
             return abi.encode(0, 0x51, hex"834100410049838201808200808101");
         }
-        // if (target == 321 && methodNum == 3275365574) {
-        //     return abi.encode(0, 0x51, hex"824400A58B0140");
-        // }
         if (target == 321 && methodNum == 3275365574) {
             return abi.encode(0, 0x51, hex"824300C10240");
         }
